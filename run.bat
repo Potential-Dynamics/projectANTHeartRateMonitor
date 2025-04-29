81e7e4f2c97eb43bc1d68232594a4d7bbf8eaace
XiaoBo <xiaobo990219@gmail.com>
2025-04-29 10:42:11 +0800

    chore: remove unnecessary line in bat script

--- run.bat
+++ run.bat
@@ -3,8 +3,5 @@
 :: Activate the virtual environment
 call .\myenv\Scripts\activate
 
-:: Change directory to 'examples'
-@REM cd examples
-
 :: Run the Python script
 python3 src\heart_rate_mqtt_broker.py
