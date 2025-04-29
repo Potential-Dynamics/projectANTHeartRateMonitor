47cd19393b924e223f4e8068f943553d3cd129a7
XiaoBo <xiaobo990219@gmail.com>
2025-04-29 15:34:36 +0800

    feat: maximum 6 ANT+ devices can be supported

--- src/heart_rate_mqtt_broker.py
+++ src/heart_rate_mqtt_broker.py
@@ -10,7 +10,10 @@ MQTT_PORT = 1883
 MQTT_TOPIC = "heart_rate/data"
 
 
-def main(device_id_0=4755, device_id_1=19983):
+
+
+#def main(device_id_0=4755, device_id_1=19983):
+def main(device_id_0=4755, device_id_1=19983, device_id_2=62618, device_id_3=26270, device_id_4= 33630, device_id_5=40571):
 
     try:
         mqtt_client = mqtt.Client()
@@ -34,23 +37,32 @@ def main(device_id_0=4755, device_id_1=19983):
 
     device0 = HeartRate(node, device_id=device_id_0)
     device1 = HeartRate(node, device_id=device_id_1)
+    device2 = HeartRate(node, device_id=device_id_2)
+    device3 = HeartRate(node, device_id=device_id_3)
+    device4 = HeartRate(node, device_id=device_id_4)
+    device5 = HeartRate(node, device_id=device_id_5)
 
     def create_callback(device_id):
         def on_device_data(page: int, page_name: str, data):
             if isinstance(data, HeartRateData):
                 heart_rate = data.heart_rate
-                print(f"Device ID {device_id}: Heart rate update {data.heart_rate}")
-                payload = {
-                        
-                        "device_id": device_id,
-                        "heart_rate": heart_rate,
-                }
-                mqtt_client.publish(MQTT_TOPIC, str(payload))
+                if heart_rate >= 50:
+                    print(f"Device ID {device_id}: Heart rate update {data.heart_rate}")
+                    payload = {
+                            
+                            "device_id": device_id,
+                            "heart_rate": heart_rate,
+                    }
+                    mqtt_client.publish(MQTT_TOPIC, str(payload))
         return on_device_data
 
     # Bind the device_id to the callback
     device0.on_device_data = create_callback(device_id_0)
     device1.on_device_data = create_callback(device_id_1)
+    device2.on_device_data = create_callback(device_id_2)
+    device3.on_device_data = create_callback(device_id_3)
+    device4.on_device_data = create_callback(device_id_4)
+    device5.on_device_data = create_callback(device_id_5)
 
     try:
         node.start()
